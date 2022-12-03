1a38932637f331c9fa061172f49d53c4796111ca
icylisper <icy@lisper.cc>
2022-12-03 12:50:22 -0800

    Adds rand-str to passel.random

--- src/passel/random.clj
+++ src/passel/random.clj
@@ -3,3 +3,13 @@
    [java.util UUID]))
 
 (defn uuid-str [] (str (UUID/randomUUID)))
+
+(defn rand-str [len]
+  (transduce
+   (map (fn [_]
+          (let [rnd (rand-int 52)]
+            (char (+ (mod rnd 26)
+                     (int (if (< rnd 26) \a \A)))))))
+   str
+   ""
+   (range len)))
