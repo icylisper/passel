6dcdbeb1d672288ea867a68208ed1fdd47073478
icylisper <icy@lisper.cc>
2022-12-06 10:22:55 -0800

    Adds error/assert - lookup registry for errors

--- src/passel/error.clj
+++ src/passel/error.clj
@@ -1,6 +1,9 @@
 (ns passel.error
+  (:require
+   [passel.log :as log])
   (:import
-   [java.io StringWriter PrintWriter]))
+   [java.io StringWriter PrintWriter]
+   [clojure.lang ExceptionInfo]))
 
 (defmacro ignore-errors
   [& body]
@@ -21,3 +24,81 @@
       (or (ex-data e#)
           {:error-id :unidentified
            :msg (.getMessage e#)}))))
+
+;; error registry
+(def ^:private registry (atom nil))
+
+(defn lookup-registry [] @registry)
+
+(defn register!
+  "Register new assertion/error identifers and associated metadata,
+  e.g.,
+
+  (register-errors!
+    {:id-not-found {:tag :precondition-failed
+                       :message \"The specified id was not found\"}})
+
+  which may later be asserted as:
+
+  (assert! (find-id id) :id-not-found)"
+  [errors]
+  (swap! registry merge
+         (if (var? errors)
+           (var-get errors)
+           errors)))
+
+(defmacro assert!
+  "Assert the truthiness of an expression, and raise an ExceptionInfo
+  exception if that fails. Supports multiple call scenarios:
+
+  (assert! (pos? -10) :not-positive-number)
+  (assert! (pos? -10) :not-positive-number {:detail \"bad number\"})
+  (assert! (pos? -10) \"Not a positive number\" :not-positive-number)
+  (assert! (pos? -10) \"Not a positive number\" :not-positive-number {:detail \"bad number\"})
+  "
+  {:arglists '([expr id]
+               [expr id ex-info-map]
+               [expr message ex-tag]
+               [expr message ex-tag ex-info-map])}
+  ([expr id]
+   `(assert! ~expr ~id {}))
+  ([expr id-or-message ex-tag-or-info-map]
+   (let [id                (when (keyword? id-or-message)
+                             id-or-message)
+         ex-info-overrides (when (map? ex-tag-or-info-map)
+                             ex-tag-or-info-map)]
+     `(or ~expr
+          (let [ex-info-map# ~(if id
+                                `(-> (lookup-registry)
+                                     (get ~id)
+                                     (merge ~ex-info-overrides)
+                                     (merge {:id ~id}))
+                                `{:message ~id-or-message
+                                  :tag     ~ex-tag-or-info-map})]
+            (throw
+             (ex-info (or (:message ex-info-map#)
+                          (format "Unknown error identifier %s" ~id-or-message))
+                      ex-info-map#))))))
+  ([expr message ex-tag ex-info-map]
+   `(or ~expr
+        (do
+          (when-let [id# (:id ~ex-info-map)]
+            (log/debug ~(merge {:log-type :error} ex-info-map)))
+          (throw
+           (ex-info ~message
+                    ~(merge {:message message
+                             :type    :assertion-failed
+                             :tag     ex-tag}
+                            ex-info-map)))))))
+
+(defmacro assert-empty!
+  "Assert that `thing` is empty, additional `args` are passed to
+  `assert!`"
+  [thing & args]
+  `(assert! (empty? ~thing) ~@args))
+
+(defmacro assert-not-empty!
+  "Assert that `thing` is not-empty, additional `args` are passed to
+  `assert!`"
+  [thing & args]
+  `(assert! (not (empty? ~thing)) ~@args))
--- test/passel/core_test.clj
+++ test/passel/core_test.clj
@@ -2,14 +2,14 @@
   (:require
    [clojure.test :refer :all]
    [passel.time :as time]
-   [passel.template :as template]
    [passel.random :as random]
    [passel.log :as log]
    [passel.json :as json]
    [passel.error :as error]
    [passel.config :as config]
    [passel.case :as case]
-   [passel.db :as db]))
+   [passel.db :as db]
+   [passel.error :as error]))
 
 (deftest ^:unit basic-test
   (log/init!)
